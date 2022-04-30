1c1e76599604a3f2a472236c2ecdc40c37643eed
João P M Morais <paulomedeiros5000@gmail.com>
2022-04-30 18:23:58 -0300

    testing css

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Home from './Home';
+import { renderTheme } from '../../styles/render-theme';
 
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', {
+    name: 'Hello',
+  }).parentElement;
+  expect(headingContainer).toHaveStyle({
+    backgroundColor: 'red',
+  });
 });
